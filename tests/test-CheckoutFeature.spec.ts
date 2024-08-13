52ce48c7a2fa480f1bcdaed45f55a77478c0db39
BriannaG <bdlgoodrich@gmail.com>
2024-08-12 21:40:26 -0600

    managing conflictc

--- tests/test-CheckoutFeature.spec.ts
+++ tests/test-CheckoutFeature.spec.ts
@@ -1,34 +1,38 @@
 
-import { test, } from '@playwright/test';
-import { InventoryPage } from './pageObjects/Inventory-Page';
-import { LoginPage } from './pageObjects/Login-Page';
-import { CartPage } from './pageObjects/Cart-Page';
-import { SideBarAndCartIcon } from './pageObjects/SideBarAndCartIcon-Page';
-import { CheckoutLoginPage } from './pageObjects/CheckoutLogin-Page';
-import { CheckoutPage } from './pageObjects/Checkout-Page';
+import { test, } from './Fixtures/docFixture';
 
+test('AddItemsAndCheckout', async ({ auth, inventoryPage, sideBarAndCartIcon, cartPage, checkoutLoginPage, checkoutPage}) => {
+  await inventoryPage.gotoInventoryPage();
+  await inventoryPage.addBackpackToCart();
+  await sideBarAndCartIcon.clickCartIcon();
+  await cartPage.verifyHeaderTitle();
+  await cartPage.clickContinueShoppingButton();
+  await inventoryPage.verifyTitle();
+  await inventoryPage.addBikeLightToCart();
+  await sideBarAndCartIcon.clickCartIcon();
+  await cartPage.verifyCartContainsBackpackAndBikeLight();
 
 
   //click checkout and cancel
-  await sauce.cartPage.clickCheckoutButton();
-  await sauce.checkoutLoginPage.clickCancelButton();
-  await sauce.cartPage.verifyHeaderTitle();
+  await cartPage.clickCheckoutButton();
+  await checkoutLoginPage.clickCancelButton();
+  await cartPage.verifyHeaderTitle();
 
   //click checkout, input info, continue, then cancel
-  await sauce.cartPage.clickCheckoutButton();
-  await sauce.checkoutLoginPage.inputLoginInfo();
-  await sauce.checkoutLoginPage.clickContinueButton();
-  await sauce.checkoutPage.verifyHeaderTitle();
-  await sauce.checkoutPage.clickCancelButton();
-  await sauce.inventoryPage.verifyHeaderTitle();
+  await cartPage.clickCheckoutButton();
+  await checkoutLoginPage.inputLoginInfo();
+  await checkoutLoginPage.clickContinueButton();
+  await checkoutPage.verifyHeaderTitle();
+  await checkoutPage.clickCancelButton();
+  await inventoryPage.verifyTitle();
 
-  await sauce.cartPage.gotoCheckoutStep1();
-  await sauce.checkoutLoginPage.inputLoginInfo();
-  await sauce.checkoutLoginPage.clickContinueButton();
-  await sauce.checkoutPage.verifyHeaderTitle();
-  await sauce.checkoutPage.clickFinishButton();
-  await sauce.checkoutPage.verifyThankYouText();
-  await sauce.checkoutPage.clickBackHomeButton();
-  await sauce.inventoryPage.verifyTitle();
+  await checkoutLoginPage.goToCheckoutLoginPage();
+  await checkoutLoginPage.inputLoginInfo();
+  await checkoutLoginPage.clickContinueButton();
+  await checkoutPage.verifyHeaderTitle();
+  await checkoutPage.clickFinishButton();
+  await checkoutPage.verifyThankYouText();
+  await checkoutPage.clickBackHomeButton();
+  await inventoryPage.verifyTitle();
 
 });
