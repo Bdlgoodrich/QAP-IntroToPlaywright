08d926ba91bf12ce26c5e71ccae9f652fad2efdd
BriannaG <bdlgoodrich@gmail.com>
2024-08-12 22:17:51 -0600

    managing merge conflict

--- tests/pageObjects/Cart-Page.ts
+++ tests/pageObjects/Cart-Page.ts
@@ -7,15 +7,13 @@ export class CartPage {
     constructor(page: Page) {
         this.page = page
     }
-<<<<<<< HEAD:tests/pageObjects/Cart-Page.ts
 
     async goToCartPage(): Promise<void> {
         await this.page.goto('/cart');
-=======
+    }
     
     async gotoCheckoutStep1() {
         await this.page.goto('/checkout-step-one.html');
->>>>>>> 39c70fd662df129b2a75c0dd4e96cc1b112ea432:tests/pageObjects/CartPage.ts
     }
 
     async verifyHeaderTitle(): Promise<void> {
